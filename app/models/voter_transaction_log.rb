aef0ed53a5d13e11327064d2fd4bcc88873b8447
jeffreyvcook <jeff.the.cook@gmail.com>
2012-05-18 23:13:17 -0700

    Re-added public/archives directory

--- app/models/voter_transaction_log.rb
+++ app/models/voter_transaction_log.rb
@@ -15,10 +15,11 @@ class VoterTransactionLog < ActiveRecord::Base
     unless (e = Election.find(eid))
       raise Exception, "Election not found during file archive operation"
     end
-    unless File.directory?(path = 'public/archives')
-      raise Exception, "No archive directory: "+path
+    apath = 'public/archives'
+    unless File.directory?(path = apath) || FileUtils.mkdir(apath)
+      raise Exception, "No archive directory: "+apath
     end
-    self.archive_name = path+"/"+"e"+eid.to_s+"_vtl_"+e.logs_max.to_s+".xml"
+    self.archive_name = apath+"/"+"e"+eid.to_s+"_vtl_"+e.logs_max.to_s+".xml"
     FileUtils.copy('public/uploads/'+self.file_name, self.archive_name)
     e.log_add(self)
     e.save
