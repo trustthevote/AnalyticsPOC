e32c8794f8e807dc74f008cc85fb8deb19ce5ce9
jeffreyvcook <jeff.the.cook@gmail.com>
2012-07-09 21:47:32 -0700

    Fixed one minor problem then precompiled assets again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @users = User.order("email ASC")
-    admin_email = AppConfig['login']['emails'][0]
-    @users.each do |u|
-      if u.email == admin_email
-        unless u.admin==true
-          u.admin=true
-          u.save
-          break
-        end
-      end
-    end
     respond_to do |format|
       format.html
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   validates :email, :presence => true, :uniqueness => true,
             :inclusion => { :in => AppConfig['login']['emails'] }
+
+  def initialize
+    super()
+    if self.email == AppConfig['login']['emails'][0]
+      self.admin = true
+    else
+      self.admin = false
+    end
+  end
+
 end
