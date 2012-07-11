02f5ffad75cbcc2f942399bfa01514739294a076
jeffreyvcook <jeff.the.cook@gmail.com>
2012-07-10 17:40:53 -0700

    Trying to get SMTP working locally

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 VoterLogAnalytics::Application.initialize!
+
+# JVC tried this to get my local SMTP server working properly
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
